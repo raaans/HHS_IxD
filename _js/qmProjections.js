aed96b74fe3ecad43e64c3c4d70abd52bc582a13
Rens <renshuizer@freedom.nl>
2020-03-01 19:00:46 +0100

    First Draft
    Added general layout and styling, added chart.js

---
+++ _js/qmProjections.js
@@ -0,0 +1,60 @@
+//get canvas to project chart in
+var ctx = document.getElementById('qmProjections').getContext('2d');
+
+Chart.defaults.global.defaultFontColor = '#fff';
+Chart.defaults.global.defaultFontFamily = 'Oxanium';
+
+//create chart
+var chart = new Chart(ctx, {
+
+    //chart type
+    type: 'line',
+
+    //insert labels
+    data: {
+        labels: ['T+1m', 'T+2m', 'T+3m', 'T+4m', 'T+5m', 'T+6m', 'T+7m', 'T+8m', 'T+9m', 'T+10m', 'T+11m', 'T+12m'],
+        datasets: [{
+            label: 'Food',
+            borderColor: '#ccac55',
+            backgroundColor: 'transparent',
+            data: [100, 94, 88, 84, 80, 75, 73]
+        }, {
+            label: 'Water',
+            borderColor: '#5575cc',
+            backgroundColor: 'transparent',
+            data: [100, 94, 87, 82, 78, 74, 70]
+        }, {
+            label: 'Fuel',
+            borderColor: '#cc7155',
+            backgroundColor: 'transparent',
+            data: [100, 80, 70, 65, 62, 60, 59]
+        }, {
+            label: 'Minimum Reserves',
+            borderColor: '#c65d3c',
+            backgroundColor: 'transparent',
+            data: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+
+        layout: {
+            padding: {
+                left: 16,
+                right: 16,
+                top: 16,
+                bottom: 16,
+            }
+        }
+    }
+});
